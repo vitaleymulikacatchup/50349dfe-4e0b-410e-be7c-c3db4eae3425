fa9f4edf22f0bcd4666860bfe597af7239f966ef
vitaleymulikacatchup <vitaley.mulika.catchup@gmail.com>
2025-10-27 14:54:27 +0200

    Update project files

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -7,7 +7,7 @@ const Hero = () => {
     }}>
       <div className="absolute inset-0 flex items-center justify-center">
         <h1 className="hero-text text-yellow-400 text-4xl md:text-6xl font-bold text-center">
-          БУХАЙ ПОКИ МОЛОДИЙ
+          Yo Yo man
         </h1>
       </div>
     </section>
